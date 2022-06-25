fcaf1f60798159e8132d1d22111888c8bd6186dd
Riyan Dharne <riyan@buffermint.com>
2022-06-26 04:15:00 +0530

    feat: external api for abusive image detection

--- app/Services/AbuseHelper.php
+++ app/Services/AbuseHelper.php
@@ -26,7 +26,31 @@ class AbuseHelper {
 
         $textResult = json_decode($response->body());
 
+        try {
+            $response = Http::withHeaders([
+                'content-type' => 'application/json',
+                'X-RapidAPI-Key' => '9e681530camsh05d62f594a121f9p1f8163jsn6d73013c4fb9',
+                'X-RapidAPI-Host' => 'nsfw-images-detection-and-classification.p.rapidapi.com'
+            ])->post('https://nsfw-images-detection-and-classification.p.rapidapi.com/adult-content', [
+                'url' => $post->image
+            ]);
+        } catch (ConnectException $e) {
+            return response()->json([
+                'error' => 'Something went wrong.'
+            ], 500);
+        }
+
+        $imageResult = json_decode($response->body());
+
         if(isset($textResult) && !strpos($textResult->abusive, 'E') && floatval($textResult->abusive) >= 0.50) {
+            Review::updateOrCreate([
+                'id' => isset($post->review) ? $post->review->id : null,
+            ], [
+                'post_id' => $post->id,
+                'abusive' => true,
+                'description' => 'Abusive content.'
+            ]);
+        } else if(isset($imageResult) && isset($imageResult->unsafe) && $imageResult->unsafe == true) {
             Review::updateOrCreate([
                 'id' => isset($post->review) ? $post->review->id : null,
             ], [
